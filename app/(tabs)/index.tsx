a704f7c38af7430ab3a4ffbb4ff2e475b26147d8
Fariz <fariz.rizaldy@salt.co.id>
2025-07-14 20:31:52 +0700

    sesi 3

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,10 @@
-import { Button, Image, ScrollView, Text, View } from 'react-native';
+import { Button, Dimensions, Platform, StyleSheet, Text, View } from 'react-native';
 
-import { Card } from '@/components/Card';
-import { Colors } from '@/constants/Colors';
 import { useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
-  const [count, setCount] = useState(10000000);
+  const [count, setCount] = useState(0);
   const [name, setName] = useState<string>('John Doe');
   const incrementCount = () => {
     setCount(count + 1);
@@ -17,82 +15,76 @@ export default function HomeScreen() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }} edges={['top']}>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={{ backgroundColor: Colors.light.background }}>
-             <View style={{backgroundColor: 'blue', flex: 0.2 }}></View>
-            <View style={{backgroundColor: 'red', flex: 0.4 }}></View>
-            <View >
-              <Text style={{fontSize: 30}}>Nomor sekarang: {count}</Text>
-              <Button onPress={incrementCount} title='Increament' />
-              <Text style={{fontSize: 30}}>Your Name: {name}</Text>
-              <Button onPress={changeName} title='Change your name' />
-            </View>
-            <View>
-              <Text>Alirkan data ke Children Component Card.tsx</Text>
-              <Card title='Ini title dari parent' desciption='Ini desciption dari parent' />
-            </View>
-            <Text style={{ ...Colors.greenBold, fontWeight: 'bold', marginTop: 40 }}>
-                Hallo world sdss
-                <Text style={{color: 'red'}}>Ini berwarna merah</Text>
-                <Text style={{fontSize: 14, fontWeight: 700}}>Ini font lebih tebal</Text>
-                Balik lagi warna hitam
-            </Text>
-            <View>
-              <Text style={{color: 'green', fontSize: 20, fontWeight: 'bold'}}>
-                Ini adalah text warna hijau
-                </Text>
-            </View>
-            <View style={{backgroundColor: 'white', flex: 0.4 }}>
-              <Image 
-                source={require('@/assets/images/react-logo.png')}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-              <Image 
-                source={require('@/assets/images/icon-computer.svg')}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-              <Image 
-                source={{
-                  uri: 'https://picsum.photos/id/237/200/300',
-                }}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-              <Image 
-                source={require('@/assets/images/react-logo.png')}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-              <Image 
-                source={require('@/assets/images/icon-computer.svg')}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-              <Image 
-                source={{
-                  uri: 'https://picsum.photos/id/237/200/300',
-                }}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-              <Image 
-                source={require('@/assets/images/react-logo.png')}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-              <Image 
-                source={require('@/assets/images/icon-computer.svg')}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-              <Image 
-                source={{
-                  uri: 'https://picsum.photos/id/237/200/300',
-                }}
-                style={{ width: 100, height: 100, alignSelf: 'center' }}
-              />
-            </View>
-          </ScrollView>
-        
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Flex Direction</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text>Test 1</Text>
+          <Text>Test 2</Text>
+        </View>
+        <Text  style={{fontSize: 20, fontWeight: 'bold'}}>Justify Content</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <Text>Test 1</Text>
+          <Text>Test 2</Text>
+          <Text>Test 3</Text>
+        </View>
+        <Text  style={{fontSize: 20, fontWeight: 'bold'}}>Align Items</Text>
+        <View style={{alignItems: 'center'}}>
+          <Text>Test 1</Text>
+          <Text>Test 2</Text>
+          <Text>Test 3</Text>
+        </View>
+        <Text  style={{fontSize: 20, fontWeight: 'bold'}}>Flex</Text>
+        <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+          <Text>Test 1</Text>
+        </View>
+        <Text  style={{fontSize: 20, fontWeight: 'bold'}}>Box</Text>
+        <View style={styles.container}>
+          <View style={[styles.box, { backgroundColor:  count > 5 ? 'yellow' : 'red'}]}/>
+          <View style={[styles.box, { backgroundColor: Platform.OS === 'ios' ? 'blue' : 'black'}]}/>
+          <View style={[styles.box, { backgroundColor: 'green'}]}/>
+        </View>
+        <Text  style={{fontSize: 20, fontWeight: 'bold'}}>Conditional Styling</Text>
+        <View>
+          <Button title="Increment Count" onPress={incrementCount} />
+          <Text style={{color: count > 10 ? 'green' : 'red'}}>
+            Count is {count}
+          </Text>
+          <Text style={{color: name === 'Fariz Rizky' ? 'blue' : 'black'}}>
+            Name is {name}
+          </Text>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const { width, height} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  box: {
+    width: width * 0.6,
+    height: Platform.OS === 'ios' ? height * 0.2 : height * 0.15,
+    margin: 10,
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
 
 
  
---
+++ constants/Box.ts
@@ -0,0 +1,5 @@
+export const Box = {
+  width: 100,
+  height: 100,
+  margin: 10,
+}
